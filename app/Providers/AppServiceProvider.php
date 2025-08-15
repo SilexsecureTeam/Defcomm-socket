99b46383bc34ec93779942916e414fd6a5b5dd1b
TheNewJosh <oloshojodamilare@gmail.com>
2025-08-15 09:36:57 +0100

    update now

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        require_once app_path('Helpers/Helpers.php');
     }
 }
