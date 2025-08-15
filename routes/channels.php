20252208b4a7ff03516ef099e280509629b7a3ba
TheNewJosh <oloshojodamilare@gmail.com>
2025-08-15 18:14:20 +0100

    updater

--- routes/channels.php
+++ routes/channels.php
@@ -14,22 +14,22 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int) $user->id === (int) decryptHelper($id);
 });
 
 Broadcast::channel('myPrivateChannel.user.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int) $user->id === (int) decryptHelper($id);
 });
 
 Broadcast::channel('chat.{receiverId}', function ($user, $receiverId) {
     // Allow only the two involved users
-    return (int) $user->id === (int) $receiverId ?? false;
+    return (int) $user->id === (int) decryptHelper($receiverId) ?? false;
 });
 
 Broadcast::channel('group.{groupId}', function ($user, $groupId) {
-    return $user->companyGroupUser()->where('group_id', $groupId)->exists();
+    return $user->companyGroupUser()->where('group_id', decryptHelper($groupId))->exists();
 });
 
 Broadcast::channel('walkie.{walkieId}', function ($user, $walkieId) {
-    return $user->wailkieTalkieSubscriber()->where('channel_id', $walkieId)->exists();
+    return $user->wailkieTalkieSubscriber()->where('channel_id', decryptHelper($walkieId))->exists();
 });
