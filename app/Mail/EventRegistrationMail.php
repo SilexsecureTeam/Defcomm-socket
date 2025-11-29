e6636efc24c1bc1f1ef647eb9c7d34c5ace1b60e
TheNewJosh <oloshojodamilare@gmail.com>
2025-11-29 08:42:04 +0100

    update up

--- app/Mail/EventRegistrationMail.php
+++ app/Mail/EventRegistrationMail.php
@@ -32,10 +32,31 @@ class EventRegistrationMail extends Mailable
         $qrData = url("/admin/form/attendance/" . encrypt($this->form->id) . "/" . encrypt($this->user->id));
 
         // Generate the QR code as a Base64 encoded string
-        $this->qrCode = base64_encode(QrCode::format('png')
+        // $this->qrCode = base64_encode(QrCode::format('png')
+        //     ->size(200)
+        //     ->margin(1)
+        //     ->generate($qrData, public_path('qr/test.png')));
+
+        $path = public_path('qr');
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        // File name
+        $fileName = time() . '_qr.png';
+
+        // Generate QR and save it as png
+        QrCode::format('png')
             ->size(200)
             ->margin(1)
-            ->generate($qrData, public_path('qr/test.png')));
+            ->generate($qrData, $path . '/' . $fileName);
+
+        // Get full path
+        $fullPath = $path . '/' . $fileName;
+
+        // Convert the saved PNG to Base64
+        $this->qrCode = base64_encode(file_get_contents($fullPath));
+        
         return $this->view('emails.eventRegistrationMail');
     }
 }
