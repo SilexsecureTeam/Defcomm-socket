fd2c79af002c2a70a0eeed9dbcf3594e30b36e80
TheNewJosh <oloshojodamilare@gmail.com>
2025-12-03 14:59:02 +0100

    updaeter

--- resources/views/super/bountyReportDetail.blade.php
+++ resources/views/super/bountyReportDetail.blade.php
@@ -39,7 +39,7 @@
                     <p><strong>Subject:</strong> {{ $data->title }}</p>
 
                     <p><strong>Details:</strong></p>
-                    <p>{!! $data->detail !!}</p>
+                    <p style="margin-left: 20px;">{!! $data->detail !!}</p>
 
                     <p><strong>Attachments:</strong></p>
                     @foreach(json_decode($data->attachment, true) as $item)
